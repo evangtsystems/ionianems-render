29b93588ded5fa3aab504f12a226dd0a496da901
evangtsystems <evangelos.lampos@gtsystems.gr>
2025-03-13 16:55:27 +0200

    9

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,20 +4,20 @@ import User from '../models/userModel.js';
 
 // User must be authenticated
 const protect = asyncHandler(async (req, res, next) => {
-  let token;
+  let token = req.cookies.jwt; // First check the token in cookies
 
-  // Read JWT from the 'jwt' cookie
-  token = req.cookies.jwt;
+  if (!token && req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    // If no cookie, check Authorization header
+    token = req.headers.authorization.split(' ')[1];
+  }
 
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
       req.user = await User.findById(decoded.userId).select('-password');
-
       next();
     } catch (error) {
-      console.error(error);
+      console.error('🚨 Token Verification Failed:', error);
       res.status(401);
       throw new Error('Not authorized, token failed');
     }
