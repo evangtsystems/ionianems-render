efd62e7b1cdac1a6f2c73dfd98387ea3e395d019
evangtsystems <evangelos.lampos@gtsystems.gr>
2025-03-13 12:00:11 +0200

    Fixed uploads folder serving in server.js

--- backend/server.js
+++ backend/server.js
@@ -78,7 +78,8 @@ if (process.env.NODE_ENV === 'production') {
   );
 } else {
   const __dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+  ;
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
