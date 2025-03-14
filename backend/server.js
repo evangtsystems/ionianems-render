45d246a38f4d527fa77bc8cc0ce4cdc76d7b7e17
evangtsystems <evangelos.lampos@gtsystems.gr>
2025-03-14 15:44:19 +0200

    /

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,8 @@ app.get('/api/config/paypal', (req, res) =>
 );
 
 // ✅ Serve Uploads Folder (Fix for Missing Images)
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'backend', 'uploads')));
+
 
 // ✅ Root API Response
 app.get('/', (req, res) => {
