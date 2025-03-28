efb64faae11b7eaf8eaf9ea6be2c9a821b37ddc7
evangtsystems <evangelos.lampos@gtsystems.gr>
2025-03-28 15:08:24 +0200

    /

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -81,7 +81,7 @@ const registerUser = asyncHandler(async (req, res) => {
     { expiresIn: EMAIL_TOKEN_EXPIRES_IN }
   );
 
-  const verificationUrl = `${process.env.FRONTEND_URL}/#/verify-email/${emailToken}`;
+  const verificationUrl = `${process.env.FRONTEND_URL}/verify-email/${emailToken}`;
 
   // Send verification email
   const message = `Please verify your email by clicking the link: ${verificationUrl}`;
